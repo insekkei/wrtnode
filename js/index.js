54247c1d05f0d447e23c96ec40db606b95c13da4
insekkei <insekkei@yahoo.com>
2015-05-20 12:25:25 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ $(function() {
 			'right':'0'
 		});
 		if($this.attr('class').match('buy')=='buy'){
+			$('#prev-step').fadeOut();
 			$('.form.buy').fadeIn();
 			$('.form.buy').siblings('form').fadeOut(0);
 		}
@@ -49,4 +50,46 @@ $(function() {
 	$('#submit-mul-form').click(function(e){
 		$('.succeed-info').fadeIn();
 	});
+
+	$('#next-step').click(function(e){
+		$('.form.buy').fadeIn();
+		$('#prev-step').fadeIn();
+		$('.form.buy').siblings('form').fadeOut(0);
+	});
+
+	$('#prev-step').click(function(e){
+		$('.form.diy').fadeIn();
+		$('.form.diy').siblings('form').fadeOut(0);
+	});
+	//select
+	jQuery.fn.select = function(options){ 
+        return this.each(function(){ 
+            var $this = $(this); 
+            var $shows = $this.find(".shows"); 
+            var $selectOption = $this.find(".selectOption"); 
+            var $el = $this.find("ul > li"); 
+                                       
+            $this.click(function(e){ 
+                $(this).toggleClass("zIndex active"); 
+                $(this).children("ul").toggleClass("dis"); 
+                e.stopPropagation(); 
+            }); 
+               
+            $el.bind("click",function(){ 
+                var $this_ = $(this); 
+                    
+                $this.find("span").removeClass("gray"); 
+                $this_.parent().parent().find(".selectOption").text($this_.text()); 
+            }); 
+               
+            $("body").bind("click",function(){ 
+                $this.removeClass("zIndex"); 
+                $this.find("ul").removeClass("dis");     
+            }) 
+               
+        //eahc End   
+        }); 
+           
+    } 
+    $(".selectContainer").select();  
 });
